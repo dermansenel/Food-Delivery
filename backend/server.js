9e303c04b95d9121a2e05f3487d292e37c9b80ed
dermansenel <dermansenel@gmail.com>
2025-01-19 03:15:06 +0300

    Setup MangoDB Database V2

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import { connectDB } from "./config/db.js";
+import foodRouter from "./routes/foodRoutes.js";
 
 // app config
 const app = express();
@@ -13,13 +14,17 @@ app.use(cors());
 //db Connection
 connectDB();
 
+//api endpoints
+app.use("/api/food", foodRouter)
+app.use("/images", express.static('uploads'))  
+
 app.get("/", (req, res) => {
     res.send("API Working");
 });
 
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`); // Use the correct variable name
-});
+    console.log(`Server is running on http://localhost:${port}`);
+}); // Kapatma parantezi eklendi
 
 
 //mongodb+srv://dermansenel:123456senel@cluster0.2iegs.mongodb.net/?
